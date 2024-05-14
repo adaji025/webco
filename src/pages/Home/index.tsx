162977e9defa49c431fdda60bbf03f23d61f796c
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2024-05-14 06:45:33 +0100

    m

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,8 @@ import { Fragment } from "react/jsx-runtime";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import AOS from "aos";
+import { MdOutlineEmail } from "react-icons/md";
+import PartnersLogo from "../../assets/partners-logo.svg";
 
 import { useEffect } from "react";
 
@@ -30,21 +32,56 @@ const Home = () => {
       </div>
 
       <div className="abuja min-h-screen py-16 flex flex-col sm:items-end mx-auto px-5 lg:px-12">
-        <h2 className="text-[40px] md:text-[84px] lg:text-[108px] font-bold sm:font-black">Our Projects</h2>
+        <h2 className="text-[40px] md:text-[84px] lg:text-[108px] font-bold sm:font-black">
+          Our Projects
+        </h2>
         <div className="grid gap-5 place-items-end mt-5 w-full">
           <div className="w-full md:w-2/3 p-3 sm:p-5 md:p-7 border-8 border-gray-600 rounded-full flex items-center justify-between">
-            <div className="font-bold sm:text-2xl lg:text-4xl">Carbon Emission Reduction</div>
+            <div className="font-bold sm:text-2xl lg:text-4xl">
+              Carbon Emission Reduction
+            </div>
             <div className="text-sm md:text-xl lg:text-2xl">Read More</div>
           </div>
           <div className="w-full md:w-2/3 p-3 sm:p-5 md:p-7 border-8 border-gray-600 rounded-full flex items-center justify-between">
-            <div className="font-bold sm:text-2xl lg:text-4xl">Carbon Emission Reduction</div>
+            <div className="font-bold sm:text-2xl lg:text-4xl">
+              Carbon Emission Reduction
+            </div>
             <div className="text-sm md:text-xl lg:text-2xl">Read More</div>
           </div>
           <div className="w-full md:w-2/3 p-3 sm:p-5 md:p-7 border-8 border-gray-600 rounded-full flex items-center justify-between">
-            <div className="font-bold sm:text-2xl lg:text-4xl">Carbon Emission Reduction</div>
+            <div className="font-bold sm:text-2xl lg:text-4xl">
+              Carbon Emission Reduction
+            </div>
             <div className="text-sm md:text-xl lg:text-2xl">Read More</div>
           </div>
         </div>
+      </div>
+
+      <div className="woman min-h-screen text-white mx-auto px-5 py-10">
+        <h2 className="text-[40px] lg:text-[64px]  font-bold sm:font-black mt-10">
+          Get In Touch
+        </h2>
+        <div className="grid gap-5 mt-5">
+          <div className="flex items-center gap-5 md:gap-10 text-xl sm:text-2xl md:text-4xl">
+            <div className="font-extrabold">Phone</div>
+            <div>(+234) 916 000 2080</div>
+          </div>
+          <div className="flex items-center gap-5 md:gap-10 text-xl sm:text-2xl md:text-4xl">
+            <div className="font-extrabold">Email</div>
+            <div>info@ecoistafrica.com</div>
+          </div>
+          <div className="flex items-center gap-5 md:gap-10 text-xl sm:text-2xl md:text-4xl">
+            <div className="font-extrabold">Address</div>
+            <div>Ikeja, Lagos, Nigeria</div>
+          </div>
+        </div>
+        <div className="mt-10 p-10 rounded-2xl bg-white text-black w-max flex items-center gap-10">
+          <MdOutlineEmail size={40} />
+          <h2 className="sm:text-[40px] font-bold">Contact Us</h2>
+        </div>
+      </div>
+      <div className="">
+        <img src={PartnersLogo} alt="" className="" />
       </div>
       <Footer />
     </Fragment>
