202a34535e0a128b01270b69d552fc0a8ec95533
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2024-05-14 05:54:18 +0100

    added the abuja bg part

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <Fragment>
       <Navbar />
-      <div className="home-banner min-h-[70vh] md:min-h-screen w-full flex items-center mt-[65px]">
+      <div className="section1 min-h-screen w-full flex items-center mt-[65px]">
         <div className="flex justify-center w-full">
           <div
             className="max-w-[800px] mx-auto px-5 lg:px-12 text-white z-10 text-center"
@@ -25,6 +25,24 @@ const Home = () => {
             <div className="mt-3 font-medium text-3xl">
               Committed to Environmental Sustainability
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="abuja min-h-screen py-16 flex flex-col sm:items-end mx-auto px-5 lg:px-12">
+        <h2 className="text-[40px] md:text-[84px] lg:text-[108px] font-bold sm:font-black">Our Projects</h2>
+        <div className="grid gap-5 place-items-end mt-5 w-full">
+          <div className="w-full md:w-2/3 p-3 sm:p-5 md:p-7 border-8 border-gray-600 rounded-full flex items-center justify-between">
+            <div className="font-bold sm:text-2xl lg:text-4xl">Carbon Emission Reduction</div>
+            <div className="text-sm md:text-xl lg:text-2xl">Read More</div>
+          </div>
+          <div className="w-full md:w-2/3 p-3 sm:p-5 md:p-7 border-8 border-gray-600 rounded-full flex items-center justify-between">
+            <div className="font-bold sm:text-2xl lg:text-4xl">Carbon Emission Reduction</div>
+            <div className="text-sm md:text-xl lg:text-2xl">Read More</div>
+          </div>
+          <div className="w-full md:w-2/3 p-3 sm:p-5 md:p-7 border-8 border-gray-600 rounded-full flex items-center justify-between">
+            <div className="font-bold sm:text-2xl lg:text-4xl">Carbon Emission Reduction</div>
+            <div className="text-sm md:text-xl lg:text-2xl">Read More</div>
           </div>
         </div>
       </div>
