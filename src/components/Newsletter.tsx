5237677e51547dd26fc5df87079415b13ed1f14c
Elisha Momoh <ellydesignconcept@gmail.com>
2024-05-28 23:12:46 +0100

    new updates

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -2,25 +2,86 @@ import React from 'react';
 
 const Newsletter = () => {
   return (
+    <div className="newsletter-container">
+
+    <div className="flex items-center gap-5 m-10 lg:mt-40">
+      <div className="flex-1">
+        <img src="src/assets/png/32.png"/>
+      </div>
+      <div className="flex-1">
+        <h2 className="font-sans font-bold text-4xl">April 22 is Earth Day!</h2>
+        <h4>by Mary - April 22, 2024     2 minute read</h4> <br/><br/>
+        <p className="font-sans text-xl">As we mark Earth Day today, it's important to ​acknowledge the pressing environmental challenges ​facing our planet. The recurring instances of extreme ​droughts and flooding worldwide serve as reminders to ​address climate change's root causes. <br/><br/>
+
+
+        To combat this environmental global crisis, concerted ​efforts are needed, encompassing sustainable ​practices, advancements in renewable energy, ​conservation initiatives, and policy reforms prioritizing ​environmental stewardship. <br/><br/>
+
+
+        By fostering a culture of responsibility and embracing ​collective action, we can pave the way towards a more ​sustainable future, preserving Earth's beauty and ​biodiversity for generations to come. <br/><br/>
+
+
+        Until then, we can truly celebrate a Happy Earth Day to ​us all!</p>
+      </div>
+    </div>
+
+    <div className="flex items-center gap-5 m-10 lg:mt-40">
+      <div className="flex-1">
+        <img src="src/assets/png/33.png"/>
+      </div>
+      <div className="flex-1">
+        <h2 className="font-sans font-bold text-4xl">Our Timeless Tradition</h2>
+        <h4>by Mary - April 03, 2024     1 minute read</h4> <br></br>
+
+        <p className="font-sans text-xl">Transitioning from traditional three-stone cooking to ​fuel-efficient cookstoves offers: </p> 
+        <ul className="list-disc list-inside font-sans text-xl">
+          <li>cost savings</li>
+          <li>improved air quality</li>
+          <li>resuced emission</li>
+          <li>less fuel consumption</li>
+        </ul>
+        <br/>
+       
+        <p className="font-sans text-xl">These stoves promote sustainability by: </p> 
+        <ul className="list-disc list-inside font-sans text-xl">
+          <li>mitigating deforestation</li>
+          <li>preserving resources</li>
+          <li>enhancing safety and efficiency on cooking</li>
+        </ul>
+        <br/>
+
+        <p className="font-sans text-xl">Overall, adopting fuel-efficient cookstoves leads to:</p> 
+        <ul className="list-disc list-inside font-sans text-xl">
+          <li>a more sustainable lifestyle</li>
+          <li>improved air quality</li>
+          <li>better quality of life</li>
+          <li>and a healthier planet</li>
+        </ul>
+
+      </div>
+    </div>
+
     <div className="flex items-center gap-5 m-10 lg:mt-40">
       <div className="flex-1">
         <img src="src/assets/png/31.png"/>
       </div>
       <div className="flex-1">
         <h2 className="font-sans font-bold text-4xl">March 22 is World Water Day!</h2>
-        <h4>by Mary - March 22, 2024     4 minute read</h4> <br/><br/>
-        <p className="font-sans">Water, as the most abundant resource on our planet, plays a ​pivotal role in sustaining life and supporting our ecosystems. ​Despite its vast availability, a pressing issue looms large: WATER ​POLLUTION. <br/><br/>
+        <h4>by Mary - March 22, 2024     4 minute read</h4> <br/>
+        <p className="font-sans text-xl">Water, as the most abundant resource on our planet, plays a ​pivotal role in sustaining life and supporting our ecosystems. ​Despite its vast availability, a pressing issue looms large: WATER ​POLLUTION. <br/>
 
 
-Far beyond being an environmental concern, water pollution ​stands as a humanitarian crisis. Countless individuals lack access ​to clean drinking water, primarily due to contamination stemming ​from industrial waste, agricultural runoff, and improper chemical ​disposal. This endangers human health and poses a threat to ​biodiversity and the overall ecological balance of Earth. <br/><br/>
+Far beyond being an environmental concern, water pollution ​stands as a humanitarian crisis. Countless individuals lack access ​to clean drinking water, primarily due to contamination stemming ​from industrial waste, agricultural runoff, and improper chemical ​disposal. This endangers human health and poses a threat to ​biodiversity and the overall ecological balance of Earth. <br/>
 
 
-To address this challenge, it is imperative that we prioritize the ​safeguarding and conservation of our water reservoirs. This entails ​the enforcement of stringent pollution control measures, ​investment in sustainable wastewater treatment technologies, and ​the promotion of conscientious water consumption practices. <br/><br/>
+To address this challenge, it is imperative that we prioritize the ​safeguarding and conservation of our water reservoirs. This entails ​the enforcement of stringent pollution control measures, ​investment in sustainable wastewater treatment technologies, and ​the promotion of conscientious water consumption practices. <br/>
 
 
 By collectively committing to these initiatives, we can pave the ​way for a future where clean, potable water is not a privilege but ​a fundamental human entitlement. Let us take decisive action now ​to protect our most precious resource for the benefit of ​generations to come.</p>
       </div>
     </div>
+    
+    </div>
+    
   );
 };
 
